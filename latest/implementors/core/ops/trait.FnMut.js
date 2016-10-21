daf8ccfa4eba17ecf0a27453cfc2a48d7157364f
Travis CI <luthaf@luthaf.fr>
2016-10-21 22:23:38 +0000

    [AUTO-COMMIT] Documentation update

--- latest/implementors/core/ops/trait.FnMut.js
+++ latest/implementors/core/ops/trait.FnMut.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["lazy_static"] = [];implementors["libc"] = [];implementors["lumol"] = [];
+implementors["lazy_static"] = [];implementors["either"] = [];implementors["libc"] = [];implementors["lumol"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
