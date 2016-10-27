98afa21f1ed434e5c1d92f456cc87c8f896ad038
Travis CI <luthaf@luthaf.fr>
2016-10-27 15:55:48 +0000

    [AUTO-COMMIT] Documentation update

--- latest/implementors/core/any/trait.Any.js
+++ latest/implementors/core/any/trait.Any.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["either"] = [];implementors["lazy_static"] = [];implementors["libc"] = [];implementors["lumol"] = [];
+implementors["lazy_static"] = [];implementors["either"] = [];implementors["libc"] = [];implementors["lumol"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
